3172cded87799b9cc3d4e342774373a2205dfe1d
Chau Phuc <chauphuc09090@gmail.com>
2026-02-25 23:56:24 +0700

    lab3 (7.1 - search feature) + update file readme

--- src/main/java/vn/edu/hcmut/cse/adse/student_management/StudentRepository.java
+++ src/main/java/vn/edu/hcmut/cse/adse/student_management/StudentRepository.java
@@ -2,5 +2,8 @@ package vn.edu.hcmut.cse.adse.student_management;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StudentRepository extends JpaRepository<Student, String> {
+    List<Student> findByNameContainingIgnoreCase(String name);
 }
